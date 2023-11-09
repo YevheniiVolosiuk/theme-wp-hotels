f4a9f8ca5cee326a351ce088a85a2baaada62517
Volosiuk Yevhenii <volosiukweb@gmail.com>
2023-11-09 01:52:36 +0100

    add custom scripts

--- assets/js/files/scroll/gotoblock.js
+++ assets/js/files/scroll/gotoblock.js
@@ -1,27 +1,28 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile, menuClose, getHash, FLS } from '../functions.js';
+import { isMobile, menuClose, getHash, FLS } from "../functions";
 // Підключення доповнення для збільшення можливостей
 // Документація: https://github.com/cferdinandi/smooth-scroll
-// import SmoothScroll from 'smooth-scroll';
+// import {SmoothScroll} from '../../libs/smoothScroll';
+
 //==============================================================================================================================================================================================================================================================================================================================
 
 // Модуль плавної проктутки до блоку
-export let gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
-  const targetBlockElement = document.querySelector(targetBlock);
+export let gotoBlock = ( targetBlock, noHeader = false, speed = 500, offsetTop = 0 ) => {
+  const targetBlockElement = document.querySelector( targetBlock );
   if (targetBlockElement) {
-    let headerItem = '';
+    let headerItem = "";
     let headerItemHeight = 0;
     if (noHeader) {
-      headerItem = 'header.header';
-      const headerElement = document.querySelector(headerItem);
-      if (!headerElement.classList.contains('_header-scroll')) {
+      headerItem = "header.header";
+      const headerElement = document.querySelector( headerItem );
+      if (!headerElement.classList.contains( "_header-scroll" )) {
         headerElement.style.cssText = `transition-duration: 0s;`;
-        headerElement.classList.add('_header-scroll');
+        headerElement.classList.add( "_header-scroll" );
         headerItemHeight = headerElement.offsetHeight;
-        headerElement.classList.remove('_header-scroll');
-        setTimeout(() => {
+        headerElement.classList.remove( "_header-scroll" );
+        setTimeout( () => {
           headerElement.style.cssText = ``;
-        }, 0);
+        }, 0 );
       } else {
         headerItemHeight = headerElement.offsetHeight;
       }
@@ -31,14 +32,14 @@ export let gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 
       speed: speed,
       header: headerItem,
       offset: offsetTop,
-      easing: 'easeOutQuad',
+      easing: "easeOutQuad"
     };
     // Закриваємо меню, якщо воно відкрите
-    document.documentElement.classList.contains('menu-open') ? menuClose() : null;
+    document.documentElement.classList.contains( "menu-open" ) ? menuClose() : null;
 
-    if (typeof SmoothScroll !== 'undefined') {
+    if (typeof SmoothScroll !== "undefined") {
       // Прокручування з використанням доповнення
-      new SmoothScroll().animateScroll(targetBlockElement, '', options);
+      new SmoothScroll().animateScroll( targetBlockElement, "", options );
     } else {
       // Прокручування стандартними засобами
       let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
@@ -46,13 +47,13 @@ export let gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 
         ? targetBlockElementPosition - headerItemHeight
         : targetBlockElementPosition;
       targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
-      window.scrollTo({
+      window.scrollTo( {
         top: targetBlockElementPosition,
-        behavior: 'smooth',
-      });
+        behavior: "smooth"
+      } );
     }
-    FLS(`[gotoBlock]: Юхуу...їдемо до ${targetBlock}`);
+    FLS( `[gotoBlock]: Юхуу...їдемо до ${targetBlock}` );
   } else {
-    FLS(`[gotoBlock]: Йой... Такого блоку немає на сторінці: ${targetBlock}`);
+    FLS( `[gotoBlock]: Йой... Такого блоку немає на сторінці: ${targetBlock}` );
   }
 };
